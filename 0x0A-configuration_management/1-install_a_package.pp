b61b2726e35a927fd6cc1c6acdbeac8de834a2b4
Fatimaezzahrarb <north.winds04@gmail.com>
2024-01-09 16:27:13 -0800

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,18 @@
 # Install a especific version of flask (2.1.0)
 
 
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
-exec {'python-installed':
-command => '/usr/bin/which python3'
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
